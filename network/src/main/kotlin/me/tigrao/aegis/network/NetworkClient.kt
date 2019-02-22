562843747162da1cfa3111348beb31117df808f1
Gabriel Costa <gabrielcosta08@gmail.com>
2019-02-21 23:37:05 -0300

    Create buildconfig variable for network api url

--- network/src/main/kotlin/me/tigrao/aegis/network/NetworkClient.kt
+++ network/src/main/kotlin/me/tigrao/aegis/network/NetworkClient.kt
@@ -1,5 +1,6 @@
 package me.tigrao.aegis.network
 
+import br.com.odete.network.BuildConfig
 import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -8,9 +9,13 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkClient {
 
+    fun <T> getApi(service: Class<T>) =
+        buildRetrofit(buildOkhttpClient())
+            .create(service)
+
     private fun buildRetrofit(okHttpClient: OkHttpClient) =
         Retrofit.Builder()
-            .baseUrl("")
+            .baseUrl(BuildConfig.API_URL)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create(Gson()))
             .build()

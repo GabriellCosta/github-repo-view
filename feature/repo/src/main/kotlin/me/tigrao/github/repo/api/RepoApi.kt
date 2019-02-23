bd16241032a20cf8dcba5adfd3d4c7c992848b3d
Gabriel Costa <gabrielcosta08@gmail.com>
2019-02-23 14:54:22 -0300

    Update RepoApi to have configurable query parameters

--- feature/repo/src/main/kotlin/me/tigrao/github/repo/api/RepoApi.kt
+++ feature/repo/src/main/kotlin/me/tigrao/github/repo/api/RepoApi.kt
@@ -4,9 +4,14 @@ import androidx.lifecycle.LiveData
 import me.tigrao.aegis.network.ui.UiState
 import me.tigrao.github.repo.data.RepositoriesResponseDTO
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 internal interface RepoApi {
 
     @GET("search/repositories?q=language:kotlin&sort=stars")
-    fun fetchRepositories(): LiveData<UiState<RepositoriesResponseDTO>>
+    fun fetchRepositories(
+        @Query("q") language: String,
+        @Query("sort") sort: String,
+        @Query("page") page: Int
+    ): LiveData<UiState<RepositoriesResponseDTO>>
 }
--- feature/repo/src/main/kotlin/me/tigrao/github/repo/api/RepoRepository.kt
+++ feature/repo/src/main/kotlin/me/tigrao/github/repo/api/RepoRepository.kt
@@ -2,5 +2,5 @@ package me.tigrao.github.repo.api
 
 internal class RepoRepository(private val api: RepoApi) {
 
-    fun fetchRepositories() = api.fetchRepositories()
+    fun fetchRepositories() = api.fetchRepositories("language:kotlin", "stars", 1)
 }

9dd6ff90fc6a4522242cf997ad56352d4e4fffaa
Gabriel Costa <gabrielcosta08@gmail.com>
2019-02-24 10:51:56 -0300

    Create transformer for Repo DTO|VO

---
+++ feature/repo/src/main/kotlin/me/tigrao/github/repo/viewmodel/RepoTransformer.kt
@@ -0,0 +1,20 @@
+package me.tigrao.github.repo.viewmodel
+
+import me.tigrao.aegis.network.ui.UiTransformer
+import me.tigrao.github.repo.data.ListItemVO
+import me.tigrao.github.repo.data.RepositoriesResponseDTO
+
+internal class RepoTransformer : UiTransformer<RepositoriesResponseDTO, List<ListItemVO>> {
+
+    override fun map(parameter: RepositoriesResponseDTO): List<ListItemVO> {
+        return parameter.items.map { map ->
+            ListItemVO(
+                map.owner.avatarUrl,
+                map.fullName,
+                map.stargazersCount,
+                map.forksCount,
+                map.description ?: ""
+            )
+        }
+    }
+}

77d6fb18274f10bbcc79131f2e5a40d776f06001
Gabriel Costa <gabrielcosta08@gmail.com>
2019-02-25 01:39:53 -0300

    Create factory for viewmodels

---
+++ feature/repo/src/main/kotlin/me/tigrao/github/repo/view/ViewModelFactory.kt
@@ -0,0 +1,20 @@
+package me.tigrao.github.repo.view
+
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.TT
+import org.kodein.di.direct
+import org.kodein.di.generic.instance
+
+class ViewModelFactory(private val kodein: Kodein) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
+        kodein.direct.Instance(TT(modelClass))
+}
+
+inline fun <reified VM : ViewModel, T> T.viewModel(): Lazy<VM> where T : KodeinAware, T : FragmentActivity {
+    return lazy { ViewModelProviders.of(this, direct.instance()).get(VM::class.java) }
+}

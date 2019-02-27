30202cb611371348008a4db9fbd73a152b7852e6
Gabriel Costa <gabrielcosta08@gmail.com>
2019-02-27 05:58:18 -0300

    Update list item layout

--- feature/repo/src/main/kotlin/me/tigrao/github/repo/data/RepositoryDTO.kt
+++ feature/repo/src/main/kotlin/me/tigrao/github/repo/data/RepositoryDTO.kt
@@ -5,8 +5,8 @@ import com.google.gson.annotations.SerializedName
 internal class RepositoryDTO(
     @SerializedName("id")
     val id: Long,
-    @SerializedName("full_name")
-    val fullName: String,
+    @SerializedName("name")
+    val name: String,
     @SerializedName("description")
     val description: String?,
     @SerializedName("owner")
--- feature/repo/src/main/kotlin/me/tigrao/github/repo/view/RepoAdapter.kt
+++ feature/repo/src/main/kotlin/me/tigrao/github/repo/view/RepoAdapter.kt
@@ -31,6 +31,7 @@ internal class RepoAdapter :
     internal class RepoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val avatar by bind<ImageView>(R.id.image_list_repo_avatar)
         private val title by bind<TextView>(R.id.txt_list_repo_title)
+        private val author by bind<TextView>(R.id.txt_list_repo_author)
         private val description by bind<TextView>(R.id.txt_list_repo_description)
         private val stars by bind<TextView>(R.id.txt_list_repo_stars)
         private val forks by bind<TextView>(R.id.txt_list_repo_fork)
@@ -40,6 +41,7 @@ internal class RepoAdapter :
             description.text = item.description
             stars.text = item.stars.toString()
             forks.text = item.forks.toString()
+            author.text = item.author
 
             Glide.with(avatar)
                 .load(item.avatar)

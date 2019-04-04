6f608c898247aaab23147fc2aaf2c26386963257
Gabriel Costa <gabrielcosta08@gmail.com>
2019-04-04 06:23:33 -0300

    Create tests for RepoRepository

---
+++ feature/repo/src/test/kotlin/me/tigrao/github/repo/api/RepoRepositoryTest.kt
@@ -0,0 +1,36 @@
+package me.tigrao.github.repo.api
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Test
+
+internal class RepoRepositoryTest {
+
+    val api = mock<RepoApi>()
+
+    val repository = RepoRepository(api)
+
+    @Test
+    fun givenARepositorySearch_WhenSuccess_ShouldDoItSortedByStars() {
+        repository.fetchRepositories(1)
+
+        verify(api).fetchRepositoriesAsync(any(), eq("stars"), any())
+    }
+
+    @Test
+    fun givenARepositorySearch_whenSuccess_ShouldDoItBySearchKotlinLanguage() {
+        repository.fetchRepositories(1)
+
+        verify(api).fetchRepositoriesAsync(eq("language:kotlin"), any(), any())
+    }
+
+    @Test
+    fun givenARepositorySearch_whenSuccess_ShouldDoItBySearchTheRequestedPage() {
+        val targetPage = 3
+        repository.fetchRepositories(targetPage)
+
+        verify(api).fetchRepositoriesAsync(any(), any(), eq(targetPage))
+    }
+}

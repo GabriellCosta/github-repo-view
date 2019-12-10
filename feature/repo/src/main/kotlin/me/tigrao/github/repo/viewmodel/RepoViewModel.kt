1062d3f5f77bd21980314cf31d6d3d357a403b8d
Gabriel Costa <gabrielcosta08@gmail.com>
2019-12-10 17:30:33 -0300

    Update RepoViewModel

--- feature/repo/src/main/kotlin/me/tigrao/github/repo/viewmodel/RepoViewModel.kt
+++ feature/repo/src/main/kotlin/me/tigrao/github/repo/viewmodel/RepoViewModel.kt
@@ -3,38 +3,34 @@ package me.tigrao.github.repo.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.paging.LivePagedListBuilder
-import androidx.paging.PagedList
 import dev.tigrao.state.StateEvent
 import io.reactivex.Scheduler
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import me.tigrao.github.repo.api.DataSourceFactory
-import me.tigrao.github.repo.data.ListItemVO
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
 import me.tigrao.github.repo.data.RepoVO
 import me.tigrao.github.repo.domain.RepoInteractor
 
-
 internal class RepoViewModel(
     private val repoInteractor: RepoInteractor,
-    private val schedulers: Scheduler) : ViewModel() {
+    private val schedulers: Scheduler
+) : ViewModel() {
 
     private val _viewState = MutableLiveData<StateEvent<RepoVO>>()
     val viewState: LiveData<StateEvent<RepoVO>> = _viewState
 
-    private var disposable: Disposable? = null
+    private var disposable = CompositeDisposable()
 
     fun fetchRepositories() {
-        disposable = repoInteractor.fetchRepos()
+        repoInteractor.fetchRepos()
             .observeOn(schedulers)
             .subscribe {
                 _viewState.value = it
-            }
+            }.addTo(disposable)
     }
 
     override fun onCleared() {
         super.onCleared()
 
-        disposable?.dispose()
+        disposable.clear()
     }
 }

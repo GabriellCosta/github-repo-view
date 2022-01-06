ae33f9f56041f0cd3d30eb6b4e47cda6644a44fc
Gabriel Costa <gabrielcosta08@gmail.com>
2022-01-05 21:06:23 -0300

    Use TYPESAFE_PROJECT_ACCESSORS

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+include(":app")
+include(":network")
+include(":styleguide")
+include(":commons-test")
+include(":feature:repo")
+include(":feature:pull")

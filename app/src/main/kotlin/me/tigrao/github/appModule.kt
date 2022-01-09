482174de399ee673b19bdfc1eb56b885835a99d7
Gabriel Costa <gabrielcosta08@gmail.com>
2022-01-09 11:23:47 -0300

    Add logger for okhttp

--- app/src/main/kotlin/me/tigrao/github/appModule.kt
+++ app/src/main/kotlin/me/tigrao/github/appModule.kt
@@ -1,10 +1,22 @@
 package me.tigrao.github
 
 import dev.tigrao.github.infra.network.NetworkBuilder
+import okhttp3.Interceptor
 import org.koin.dsl.module
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.bind
 
 val appModule = module {
     single {
         NetworkBuilder(BuildConfig.API_URL)
     }
+
+    factory {
+        HttpLoggingInterceptor()
+            .also {
+                if (BuildConfig.DEBUG) {
+                    it.level = HttpLoggingInterceptor.Level.BODY
+                }
+            }
+    } bind Interceptor::class
 }
